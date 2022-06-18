c4d20ff2215778aa80f561b766c9c917b4c70d3c
Sumeet Mathapti <sumeet.mathpati@gmail.com>
2022-06-18 10:31:33 +0000

    Implemented root route.

--- go-fibre-es7/main.go
+++ go-fibre-es7/main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"strings"
 
+	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/gofiber/fiber/v2"
 )
 
 const (
-	HOST = "localhost"
-	PORT = "5000"
+	HOST  = "0.0.0.0"
+	PORT  = "5000"
+	INDEX = "events"
 )
 
 func main() {
+	var r map[string]interface{}
 	app := fiber.New()
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+
+		res, err := es.Index(
+			INDEX,
+			strings.NewReader(`{"title" : "events"}`),
+			es.Index.WithDocumentID("1"),
+			es.Index.WithRefresh("true"),
+		)
+		if err != nil {
+			log.Fatalf("Error getting response: %s", err)
+		}
+
+		res, err = es.Count(es.Count.WithIndex(INDEX))
+		json.NewDecoder(res.Body).Decode(&r)
+
+		return c.JSON(struct {
+			EventCount int `json:"EventCount"`
+		}{
+			EventCount: int(r["count"].(float64)),
+		})
 	})
 
 	log.Fatal(app.Listen(HOST + ":" + PORT))
